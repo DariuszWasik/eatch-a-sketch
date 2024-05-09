c1718e6a3eac04630ec855d9dc6c9467ab6501cd
Dario_W <wasik.dariusz24@gmail.com>
2024-05-09 23:39:41 +0200

    create button on the top of a site

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // create webpage with 16x16 grid of square divs
 
 const mainDiv = document.querySelector("#mainDiv");
-const sideDimension = 33;
+const sideDimension = 16;
 for (let i=0; i<sideDimension*sideDimension; i++) {
     var square = document.createElement("div");
     square.className = "sqr";
@@ -18,7 +18,12 @@ for (let i=0; i<sideDimension*sideDimension; i++) {
 var allSquares = document.querySelectorAll(".sqr")
 allSquares.forEach((el) => el.addEventListener("mouseover", f => {el.style.background= "red"}))
 
+// add button that asks about the square numbers
 
+const btn = document.createElement("button");
+btn.innerHTML = "click me to set how many squares do you want  to have? "
+btn.style = "font-size: 25px; padding: 20px; border: 5px solid black; background-color: silver "
+document.body.insertBefore(btn, mainDiv)
 
 console.log("div dimension", mainDiv.offsetWidth)
 console.log("square dimension", square.offsetWidth)
