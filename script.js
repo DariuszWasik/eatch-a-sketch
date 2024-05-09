932296ced1f08b9cd024f71da4ec7afbf72ea3cb
Dario_W <wasik.dariusz24@gmail.com>
2024-05-09 23:04:20 +0200

    add hovering feature

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // create webpage with 16x16 grid of square divs
 
 const mainDiv = document.querySelector("#mainDiv");
-const sideDimension = 21;
+const sideDimension = 33;
 for (let i=0; i<sideDimension*sideDimension; i++) {
     var square = document.createElement("div");
     square.className = "sqr";
@@ -10,6 +10,14 @@ for (let i=0; i<sideDimension*sideDimension; i++) {
     square.style.width = `${(mainDiv.offsetWidth/sideDimension)-2.4}px`
     mainDiv.appendChild(square);
 }
+
+
+
+// add listener that changes square color while hovering
+
+var allSquares = document.querySelectorAll(".sqr")
+allSquares.forEach((el) => el.addEventListener("mouseover", f => {el.style.background= "red"}))
+
 
 
 console.log("div dimension", mainDiv.offsetWidth)
